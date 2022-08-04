b3380360ab8c906bf4891374f3d22258631aea24
Miguel Geiss Arnhold <arnhold.mg@gmail.com>
2022-08-04 09:06:39 -0300

    env prepare

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -4,4 +4,6 @@ cd /var/www/appdata &&
 composer update &&
 chgrp -R www-data storage bootstrap/cache &&
 chmod -R ug+rwx storage bootstrap/cache &&
+php artisan optimize:clear &&
+php artisan optimize &&
 php-fpm
